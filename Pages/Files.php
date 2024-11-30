0240c670d423f23a2e3ae8679791d65296000458
Tomáš Bosák <tombos255@gmail.com>
2024-11-30 02:26:06 +0100

    Refactoring

--- Pages/Files.php
+++ Pages/Files.php
@@ -31,8 +31,8 @@ if (!$queryResult) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/styles.css">
-    <link rel="stylesheet" href="../CSS/input.css">
+    <link rel="stylesheet" href="../themes/styles.css">
+    <link rel="stylesheet" href="../themes/input.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 
     <script type="text/javascript" src="../JavaScript/Main.js"></script>
--- Pages/UserAuth.php
+++ Pages/UserAuth.php
@@ -58,7 +58,7 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/styles.css">
+    <link rel="stylesheet" href="../themes/styles.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 
     <script type="text/javascript" src="../JavaScript/Main.js"></script>
